95c9aeb6a2bf9470a21d14145d02ecd9b49ee1bb
Salem Asrat <salemasrat@gmail.com>
2019-09-28 22:44:06 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -2,13 +2,13 @@
 def hopper
 	programmer_hash = 
  		{
-        :grace_hopper => {
+          :grace_hopper =>
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
+        :alan_kay = {
+          known_for: ["Object Orientation"]
+          languages = ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
           :known_for => "Unix",
