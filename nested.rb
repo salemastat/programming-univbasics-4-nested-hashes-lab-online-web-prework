a7f3a5bfa45ddb08381ffcaa9b089497ef7ebf26
Salem Asrat <salemasrat@gmail.com>
2019-09-28 22:43:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -7,7 +7,7 @@ def hopper
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay = {
-          known_for : "Object Orientation",
+          known_for: ["Object Orientation",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
